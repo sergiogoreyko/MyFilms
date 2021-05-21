31448620abd600010e8cf6696737f2646b7feca2
Sergei <sergiogoreyko99@ya.ru>
2021-05-21 19:29:42 +0300

    Initial Commit

---
+++ MyFilms/MyFilmsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFilmsApp.swift
+//  MyFilms
+//
+//  Created by Сергей Горейко on 21.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct MyFilmsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
